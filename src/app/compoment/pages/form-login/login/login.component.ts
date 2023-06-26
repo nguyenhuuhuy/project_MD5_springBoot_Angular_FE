c07a8be69554f973924ee513cb2687537ac590cf
nguyenhuuhuy <huy.cb96@gmail.com>
2023-06-26 09:44:33 +0700

    config

--- src/app/compoment/pages/form-login/login/login.component.ts
+++ src/app/compoment/pages/form-login/login/login.component.ts
@@ -9,43 +9,49 @@ import {SignInForm} from "../../../../model/SignInForm";
   templateUrl: './login.component.html',
   styleUrls: ['./login.component.scss']
 })
-export class LoginComponent implements OnInit{
+export class LoginComponent implements OnInit {
 
   hide = true;
-  form:any = {};
+  form: any = {};
   signInForm?: SignInForm;
   status = 'Please fill in the form to login!';
-  statusError = '___________________________________________';
+
   constructor(private authService: AuthService,
               private tokenService: TokenService,
               private router: Router) {
   }
+
   login() {
     this.signInForm = new SignInForm(
       this.form.username,
       this.form.password
     )
-    this.authService.signIn(this.signInForm).subscribe(data =>{
+    this.authService.signIn(this.signInForm).subscribe(data => {
       // @ts-ignore
-      if(data.status==202){
-        this.statusError = 'Login failed! Please check your account !'
-      } else {
-        // @ts-ignore
-        this.tokenService.setName(data.name);
-        // @ts-ignore
-        this.tokenService.setAvatar(data.avatar);
-        // @ts-ignore
-        this.tokenService.setToken(data.token);
-        // @ts-ignore
-        this.tokenService.setRole(data.roles);
-        this.router.navigate(['']).then(()=>{
-          window.location.reload();
-        })
+      if (data.status == 202) {
+        this.status = 'Login failed! Please check your account !'
+      } else { // @ts-ignore
+        if (data.message == 'login_denied') {
+                this.status = 'bạn đã bị khóa nick'
+        } else {
+          // @ts-ignore
+          this.tokenService.setName(data.name);
+          // @ts-ignore
+          this.tokenService.setAvatar(data.avatar);
+          // @ts-ignore
+          this.tokenService.setToken(data.token);
+          // @ts-ignore
+          this.tokenService.setRole(data.roles);
+          this.router.navigate(['']).then(() => {
+            window.location.reload();
+          })
+        }
       }
     })
   }
+
   ngOnInit(): void {
-    if (this.authService.checkRegister){
+    if (this.authService.checkRegister) {
       this.status = 'ĐĂNG NHẬP THÀNH CÔNG'
     }
   }
--- src/app/model/SignInForm.ts
+++ src/app/model/SignInForm.ts
@@ -1,6 +1,8 @@
 export class SignInForm {
   private username?: string;
   private password?: string;
+  
+
 
   constructor(username: string, password: string) {
     this.username = username;
